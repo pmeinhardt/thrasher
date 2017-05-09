b37730f8056747716834c7265ab104c172aa8b6c
Robert J <chew-z@users.noreply.github.com>
2017-05-09 17:07:53 +0800

    Remove ignored folders and files

--- jxa/iTunes_Library_tracks.js
+++
@@ -1,6 +0,0 @@
-function run(argv) { 
-	let app = Application('iTunes'); 
-	let lib = app.playlists.byName('Library'); 
-	let tracks = lib.tracks(); //.filter(function (t) { return t.class() === 'fileTrack';})
-	return JSON.stringify(tracks.map(function (t) { return {id: t.id(), name: t.name(), collection: t.album(), artist: t.artist()}; })); 
-}
